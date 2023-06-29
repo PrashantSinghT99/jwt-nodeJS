27b85672c7e600e3c8a754252425506a4416d7a6
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-06-29 19:57:56 +0530

    jwt token

--- server.js
+++ server.js
@@ -3,29 +3,41 @@ const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
 app.use(express.json())
-
-
 const posts = [
     {
-        username: "User1",
-        title: "Post 1"
+        username: 'Kyle',
+        title: 'Post 1'
     },
     {
-        username: "User2",
-        title: "Post 2"
-    },
+        username: 'Jim',
+        title: 'Post 2'
+    }
 ]
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter((post) => post.username === req.user.name))
 })
 
 app.post('/login', (req, res) => {
-    //Authenticate user
+    //Authenticate user assuming user valid
     const username = req.body.username;
     const user = { name: username }
     const accessToken = jwt.sign(user, process.env.SECRET_KEY)
     res.json({ accessToken: accessToken })
 })
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user;
+        next()
+    })
+
+}
+
 app.listen(3000)
+
