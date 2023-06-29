42780ca39dff98994c403a0ccc568f185bfe5e6d
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-06-29 18:50:53 +0530

    initial setup

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const express = require("express")
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+app.use(express.json())
+
+
 const posts = [
     {
-        username: "User 1",
+        username: "User1",
         title: "Post 1"
     },
     {
-        username: "User 2",
+        username: "User2",
         title: "Post 2"
     },
 ]
 
 app.get('/posts', (req, res) => {
-res.json(posts)
+    res.json(posts)
+})
+
+app.post('/login', (req, res) => {
+    //Authenticate user
+    const username = req.body.username;
+    const user = { name: username }
+    const accessToken = jwt.sign(user, process.env.SECRET_KEY)
+    res.json({ accessToken: accessToken })
 })
 
 app.listen(3000)
