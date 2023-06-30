b790eecc4a4896aa2b666d73dae61dc8eeaf4f58
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-06-30 19:31:17 +0530

    undefined req.user.name fix

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
+
 app.use(express.json())
+
 const posts = [
     {
         username: 'Kyle',
@@ -15,7 +18,7 @@ const posts = [
 ]
 
 app.get('/posts', authenticateToken, (req, res) => {
-    res.json(posts.filter((post) => post.username === req.user.name))
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req, res) => {
@@ -29,12 +32,13 @@ app.post('/login', (req, res) => {
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.sendStatus(401);
-
+    if (token == null) return res.sendStatus(401)
+  
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-        if (err) return res.sendStatus(403)
-        req.user = user;
-        next()
+      //console.log(err)
+      if (err) return res.sendStatus(403)
+      req.user = user
+      next()
     })
 
 }
